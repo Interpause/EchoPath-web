1986d769ea65ce0ce5be126528a909cfa018faa9
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2026-03-14 23:15:42 -0400

    feat: init capacitor for android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'echopath.interpause.github.io',
+  appName: 'EchoPath',
+  webDir: './dist/'
+};
+
+export default config;
